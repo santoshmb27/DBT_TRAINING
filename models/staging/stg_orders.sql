f1543ade1796c9e41bd6968e7599443e460a2dc8
Santosh M B <santhoshmb22@gmail.com>
2025-06-19 06:59:06 +0000

    developed report_profit_by_customer and report_profit_by_product

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -8,10 +8,12 @@ o.ordersellingprice - ordercostprice as orderprofit,
 o.ordercostprice,
 o.ordersellingprice,
 --from raw customer
+c.customerid,
 c.customername,
 c.segment,
 c.country,
 --from raw product
+p.productid,
 p.category,
 p.productname,
 p.subcategory
